744eb96991fa1fc64d1c465c0f90600b4a5d6a0e
Muhammad Osama <imuhammadosama@icloud.com>
2022-12-04 18:36:19 +0500

    Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,36 @@
+import './Home.css';
+import { icons } from '../data/anron';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='content'>
+      <div className='header'>
+        <div className='logo'>
+          <img src='http://res.cloudinary.com/dvwpbbisf/image/upload/q_auto:eco/v1670158090/ttcvdqvu3vpziejf5hvw.webp' />
+        </div>
+        <div className='search'>
+          <input className='search-input' placeholder='Search an icon...' />
+        </div>
+      </div>
+      <div className='icons'>
+        {icons.map((icon) => {
+          return (
+            <div className='icon'>
+              <div className='icon-svg'>
+                <span
+                  id={`icon`}
+                  dangerouslySetInnerHTML={{ __html: icon.svg }}
+                />
+              </div>
+              <div className='icon-name'>{icon.name}</div>
+              <div className='icon-action animated animatedFadeInUp fadeInUp'>
+                <div className='icon-copy'>Copy</div>
+                <div className='icon-download'>Download</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
