1de3b889b9e5fe47e0c8ef82891f0e657507d9b9
Muhammad Osama <imuhammadosama@icloud.com>
2022-12-14 21:09:21 +0500

    Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,23 @@
+import searchIcon from '../assets/icons/search.svg';
+import '../assets/styles/search.css';
+import '../assets/styles/icons.css';
+
 export default function Home() {
-  return <div>Apples</div>;
+  return (
+    <div>
+      {/* Search Field Starts */}
+      <div className='search'>
+        <div className='search-field'>
+          <input id='search-input' placeholder='Search icons'></input>
+          <img src={searchIcon} id='search-icon' />
+        </div>
+      </div>
+      {/* Search Field Ends */}
+
+      <div id='icons'>
+        <div id='icons-filter'></div>
+        <div id='icons-images'></div>
+      </div>
+    </div>
+  );
 }
